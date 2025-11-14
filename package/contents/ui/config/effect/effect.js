196f48c6d417068df66945470d028f6aac5508bf
Sadi Yumuşak <Sadi58@users.noreply.github.com>
2025-11-14 16:59:42 +0300

    Remove unused ActiveWindowString and Regexp conditions
    Removed unused condition types and their implementations.

--- package/contents/ui/config/effect/effect.js
+++ package/contents/ui/config/effect/effect.js
@@ -5,8 +5,6 @@
  */
 
 const CONDITION_ACTIVE_WINDOW_FLAG = 0;
-const CONDITION_ACTIVE_WINDOW_STRING = 1;
-const CONDITION_ACTIVE_WINDOW_REGEX = 2;
 
 class Condition {
     constructor(effectIndex) {
@@ -21,10 +19,6 @@ class Condition {
         switch (model.conditionType) {
             case CONDITION_ACTIVE_WINDOW_FLAG:
                 return new ActiveWindowFlagCondition(model.effectIndex, model.arg0, !!model.arg1);
-            case CONDITION_ACTIVE_WINDOW_STRING:
-                return new ActiveWindowStringCondition(model.effectIndex, model.arg0, model.arg1);
-            case CONDITION_ACTIVE_WINDOW_REGEX:
-                return new ActiveWindowRegexpCondition(model.effectIndex, model.arg0, model.arg1);
             default:
                 throw new Error("Unsupported condition type: " + model.conditionType);
         }
@@ -54,30 +48,5 @@ class ActiveWindowFlagCondition extends ActiveWindowCondition {
 
     checkActiveWindowCondition(activeWindow) {
         return activeWindow[this.propertyName] === this.propertyValue;
-    }
-}
-
-class ActiveWindowStringCondition extends ActiveWindowCondition {
-    constructor(effectIndex, propertyName, propertyValue) {
-        super(effectIndex);
-        this.propertyName = propertyName;
-        this.propertyValue = propertyValue;
-    }
-
-    checkActiveWindowCondition(activeWindow) {
-        return activeWindow[this.propertyName] === this.propertyValue;
-    }
-}
-
-class ActiveWindowRegexpCondition extends ActiveWindowCondition {
-    constructor(effectIndex, propertyName, regExp) {
-        super(effectIndex);
-        this.propertyName = propertyName;
-        this.regExp = new RegExp(regExp, "g");
-    }
-
-    checkActiveWindowCondition(activeWindow) {
-        const value = activeWindow[this.propertyName];
-        return (value === undefined || value === null) ? false : value.match(this.regExp);
     }
 }
